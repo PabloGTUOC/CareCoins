58930a0228d0bf76e6d10a8f51448c46d8a1d4cb
Pablo Gonzalez Torres <105376688+PabloGTUOC@users.noreply.github.com>
2025-06-20 13:25:39 +0200

    Add family setup flow

--- lib/services/auth_gate.dart
+++ lib/services/auth_gate.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../screens/home_page.dart';
 import '../screens/landing_page.dart';
+import '../screens/family_setup_page.dart';
 
 class AuthGate extends StatefulWidget {
   const AuthGate({super.key});
@@ -12,6 +13,7 @@ class AuthGate extends StatefulWidget {
 
 class _AuthGateState extends State<AuthGate> {
   bool _checkingProfile = false;
+  bool _needsFamilySetup = false;
 
   @override
   void initState() {
@@ -33,7 +35,7 @@ class _AuthGateState extends State<AuthGate> {
     // Check if user exists in our users table
     final existing = await Supabase.instance.client
         .from('users')
-        .select()
+        .select('family_id')
         .eq('id', user.id)
         .maybeSingle();
 
@@ -43,6 +45,9 @@ class _AuthGateState extends State<AuthGate> {
         'id': user.id,
         'email': user.email,
       });
+      _needsFamilySetup = true;
+    } else {
+      _needsFamilySetup = existing['family_id'] == null;
     }
 
     // Insert login_history record
@@ -64,6 +69,8 @@ class _AuthGateState extends State<AuthGate> {
 
     if (session == null) {
       return const LandingPage();
+    } else if (_needsFamilySetup) {
+      return const FamilySetupPage();
     } else {
       return const HomePage();
     }
